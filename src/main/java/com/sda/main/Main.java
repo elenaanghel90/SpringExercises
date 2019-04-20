435ccd754b4821f45da386e64370ddeb70402749
Elena Anghel <elenamarianaanghel@gmail.com>
2019-04-20 12:04:37 +0300

    adding beans for tool and workerMotto

--- src/main/java/com/sda/main/Main.java
+++ src/main/java/com/sda/main/Main.java
@@ -1,6 +1,8 @@
 package com.sda.main;
 
+import com.sda.production.Tool;
 import com.sda.production.Worker;
+import com.sda.production.WorkerMotto;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,6 +12,11 @@ public class Main {
                 "worker.xml", "tools.xml"};
         ApplicationContext xmlapplicationContext = new ClassPathXmlApplicationContext(beans);
         Worker testWorker = xmlapplicationContext.getBean(Worker.class);
-        System.out.println("Worker with age: " + testWorker.getAge() + ", with profession: " + testWorker.getProfession());
+        Tool testTool = xmlapplicationContext.getBean(Tool.class);
+        WorkerMotto testWorkerMoto = xmlapplicationContext.getBean(WorkerMotto.class);
+        System.out.println("Worker with age: " + testWorker.getAge() + ", with profession: "
+                + testWorker.getProfession() + ", has the tool " + testTool.getName() +" with weight "+ testTool.getWeight() +
+                ". The worker has the next citate: "+ testWorkerMoto.getCitate());
+
     }
 }
